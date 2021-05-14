6a8b30b19f94bb1168e7dce3e68a4dfb582db0d4
sportyfred <ivanboring@gmail.com>
2021-05-14 20:24:10 +0200

    dfj

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/index.css';
 
+const fs = require('fs');
+const pdf = require('pdf-parse');
+ 
+let dataBuffer = fs.readFileSync('path to PDF file...');
+ 
+pdf(dataBuffer).then(function(data) {
+ 
+    // number of pages
+    console.log(data.numpages);
+    // number of rendered pages
+    console.log(data.numrender);
+    // PDF info
+    console.log(data.info);
+    // PDF metadata
+    console.log(data.metadata); 
+    // PDF.js version
+    // check https://mozilla.github.io/pdf.js/getting_started/
+    console.log(data.version);
+    // PDF text
+    console.log(data.text); 
+        
+});
+
 function Index() {
   const [date, setDate] = useState(null);
   useEffect(() => {
